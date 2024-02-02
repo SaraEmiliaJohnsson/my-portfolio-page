f448f5e50b790b9a2eb4db1710d4796f190b6546
Sara Johnsson <sara86persson@gmail.com>
2024-02-02 10:28:01 +0100

    added pictures to new repos

--- src/components/api/GitHubApi.jsx
+++ src/components/api/GitHubApi.jsx
@@ -7,17 +7,21 @@ import project1Image from "./images/cafecreme.png";
 import project2Image from "./images/christmascard.png";
 import project3Image from "./images/klonaensida.png";
 import project4Image from "./images/memorygame.png";
-import project5Image from "./images/parmarochpoesi.png";
-import project6Image from "./images/taverna.png";
+import project5Image from "./images/my-portfolio-page.png";
+import project6Image from "./images/parmarochpoesi.png";
+import project7Image from "./images/portfolio-website.png";
+import project8Image from "./images/taverna.png";
 
 // Define a mapping between project IDs and their respective images
 const imageMapping = {
 	745169602: project1Image,
 	735361949: project2Image,
-	700987599: project2Image,
+	700987599: project3Image,
 	725944709: project4Image,
-	707613613: project5Image,
-	734480971: project6Image,
+	751506389: project5Image,
+	707613613: project6Image,
+	751482233: project7Image,
+	734480971: project8Image,
 };
 
 // GitHubApi is a functional component that fetches GitHub project data and displays it.
