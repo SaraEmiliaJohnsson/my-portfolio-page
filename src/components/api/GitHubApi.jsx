3a34fdf4ee5159f107f0a6c254d7d4a3d132dbd1
Sara Johnsson <sara86persson@hotmail.com>
2024-05-19 22:13:20 +0200

    styling fix

--- src/components/api/GitHubApi.jsx
+++ src/components/api/GitHubApi.jsx
@@ -2,27 +2,19 @@ import React from "react";
 import { useEffect, useState } from "react";
 import "./GitHubApi.scss";
 
-// Import images
-import project1Image from "./images/cafecreme.png";
-import project2Image from "./images/christmascard.png";
-import project3Image from "./images/klonaensida.png";
-import project4Image from "./images/memorygame.png";
-import project5Image from "./images/my-portfolio-page.png";
-import project6Image from "./images/parmarochpoesi.png";
-import project7Image from "./images/portfolio-website.png";
-import project8Image from "./images/taverna.png";
 
-// Define a mapping between project IDs and their respective images
-const imageMapping = {
-	745169602: project1Image,
-	735361949: project2Image,
-	700987599: project3Image,
-	725944709: project4Image,
-	751506389: project5Image,
-	707613613: project6Image,
-	751482233: project7Image,
-	734480971: project8Image,
-};
+
+// // Define a mapping between project IDs and their respective images
+// const imageMapping = {
+// 	745169602: project1Image,
+// 	735361949: project2Image,
+// 	700987599: project3Image,
+// 	725944709: project4Image,
+// 	751506389: project5Image,
+// 	707613613: project6Image,
+// 	751482233: project7Image,
+// 	734480971: project8Image,
+// };
 
 // GitHubApi is a functional component that fetches GitHub project data and displays it.
 export default function GitHubApi() {
@@ -41,26 +33,17 @@ export default function GitHubApi() {
 				// Parse the response as JSON
 				const projects = await response.json();
 
-				// Map each project with its corresponding image using the imageMapping
-				const projectsWithImages = projects.map((project) => {
-					const projectId = project.id;
-					// Retrieve the image based on the project's ID
-					let image = imageMapping[projectId];
 
-					return {
-						...project,
-						image_url: image,
-					};
-				});
+
 
 				// Set the state with the processed data
-				setData(projectsWithImages);
+				setData(projects);
 				console.log(projectsWithImages);
 			} catch (error) {
 				// Handle errors if any occur during the fetch process
 				console.error("Error fetching data:", error);
 			}
-		};
+		}
 
 		// Call the fetchData function when the component mounts
 		fetchData();
@@ -82,13 +65,8 @@ export default function GitHubApi() {
 								<h3>{repo.name}</h3>
 							</a>
 
-							<p>{repo.description}</p>
-							<img
-								key={repo.id}
-								src={repo.image_url}
-								alt={repo.name}
-								className="project-image"
-							/>
+
+
 						</article>
 					))}
 				</section>
